a34b88d183497d84a0ce7ee409042b874808f367
SailorVirgo <tm11solarbeam@gmail.com>
2024-08-04 23:57:59 -0500

    ready to go

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,15 +1,18 @@
-const logout = async () => {
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+document.addEventListener('DOMContentLoaded', (event) => {
+  const logoutButton = document.querySelector('#logout');
+  
+  if (logoutButton) {
+    logoutButton.addEventListener('click', async () => {
+      const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log out');
+      }
     });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert('Failed to log out');
-    }
-  };
-  
-  document.querySelector('#logout').addEventListener('click', logout);
-  
+  }
+});
